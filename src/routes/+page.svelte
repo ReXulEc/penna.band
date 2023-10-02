4d1c0973f6263e9739f9918e9559c6dad2b728f5
ReXulEc <mertdoguonder@protonmail.com>
2023-10-02 23:02:07 +0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import photo from '../lib/images/logo.png'
+	import photo1 from '../lib/images/1.jpg'
+	import photo2 from '../lib/images/2.png'
+	import photo3 from '../lib/images/3.png'
+	import photo4 from '../lib/images/4.png'
+	import photo5 from '../lib/images/5.jpg'
+	import photo6 from '../lib/images/6.jpg'
+
+
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div>
+	<div class="h-screen w-full flex justify-center items-center">
+		<img src={photo} class="md:w-2/12 w-6/12" alt="">
+	</div>
+</div>
+
