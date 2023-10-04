3dd66ed1b79bfa4b8a0c1472a9e7eda7f6ae1c02
ReXulEc <mertdoguonder@protonmail.com>
2023-10-04 23:18:18 +0300

    feat: href

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -7,9 +7,9 @@
     <div class="flex justify-center">
         <div class="md:w-6/12 w-5/6 flex justify-between volkorn text-lg font-semibold">
             <a href="">Home</a>
-            <a href="albums">Albums</a>
-            <a>Live</a>
-            <a>Merch</a>
+            <a href="">Albums</a>
+            <a href="">Live</a>
+            <a href="">Merch</a>
         </div>
     </div>
     
